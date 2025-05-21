e6d12b263f0f99648752ce756bdde63208fdca0e
bsf91 <brunoscartezinifukuda@gmail.com>
2025-05-21 16:00:18 -0300

    Criando o hero e importando a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const dartSass = require('gulp-dart-sass');
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,10 +8,18 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
+
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
 function watch() {
     gulp.watch('./src/styles/*.scss', styles);
 }
 
+exports.build = gulp.series(styles, images);
 exports.styles = styles;
 exports.watch = watch;
-exports.default = gulp.series(styles, watch);
+exports.default = gulp.series(styles, images);
