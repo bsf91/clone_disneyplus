a17f5667da7d0b7e8d6b2a7f64f26255a65eb2f4
bsf91 <brunoscartezinifukuda@gmail.com>
2025-05-29 13:37:16 -0300

    Configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,40 @@
 const gulp = require('gulp');
 const dartSass = require('gulp-dart-sass');
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
+// Minificar JavaScript
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+// Compilar SCSS
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(dartSass({ outputStyle: 'compressed' }).on('error', dartSass.logError))
         .pipe(gulp.dest('./dist/css'));
 }
 
-
+// Otimizar imagens
 function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/images'));
 }
 
+// Assistir mudanças
 function watch() {
     gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/scripts/*.js', scripts);
+    gulp.watch('./src/images/**/*', images);
 }
 
-exports.build = gulp.series(styles, images);
+// Exportações
+exports.build = gulp.series(styles, images, scripts);
 exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
 exports.watch = watch;
-exports.default = gulp.series(styles, images);
+exports.default = gulp.series(styles, images, scripts, watch);
